915d077f6ae1e87d3c3e3fdcd3b43b8f71d0d529
Ahmed Ragab <89437174+Ahmedragab24@users.noreply.github.com>
2023-11-19 00:23:21 +0200

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,7 +96,7 @@ const sr = ScrollReveal({
     distance: '60px',
     duration: 2500,
     delay: 400,
-    reset: true,
+    // reset: true,
 })
 
 sr.reveal(`.home--subtitle , .skills--data, .section--subtitle`)
